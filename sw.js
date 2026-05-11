ebcb21126b66b4527524c93ea76f7367db8f2176
academialuchorolandopadel <abcconstruccionesenseco@gmail.com>
2026-05-10 22:42:52 -0300

    Upgrade service worker cache to version 3
    Updated cache version and added new files to cache.

--- sw.js
+++ sw.js
@@ -1,18 +1,34 @@
-const CACHE_NAME = 'padel-categorizacion-v1';
+const CACHE_NAME = 'padel-categorizacion-v3';
 const urlsToCache = [
   'index.html',
   'styles.css',
   'data.js',
   'ejercicios.js',
   'app.js',
+  'auth-ui.js',
+  'firebase-config.js',
   '1000001630.png',
   'manifest.json'
 ];
 
 self.addEventListener('install', event => {
+  // Forzar activación inmediata sin esperar tabs viejas
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  // Borrar todos los caches viejos
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
